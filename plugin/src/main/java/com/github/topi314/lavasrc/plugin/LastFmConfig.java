7c5555a580e1526798c507226ec712825d8c3cd3
bobandbobby <ctrlwaslastinflorida@gmail.com>
2025-02-02 17:40:44 -0500

    Update LastFmConfig.java

--- plugin/src/main/java/com/github/topi314/lavasrc/plugin/LastFmConfig.java
+++ plugin/src/main/java/com/github/topi314/lavasrc/plugin/LastFmConfig.java
@@ -1,1 +1,46 @@
+package com.github.topi314.lavasrc.plugin;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "plugins.lavasrc.lastfm")
+@Component
+public class LastFmConfig {
+
+    private String apiKey;
+    private String apiSecret;
+    private String username;
+    private String passwordHash;
+
+    public String getApiKey() {
+        return this.apiKey;
+    }
+
+    public String getApiSecret() {
+        return this.apiSecret;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPasswordHash() {
+        return this.passwordHash;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
